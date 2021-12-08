2597d9ce93b03a77e101a25c0480fa148096a139
Jhin <88294616+Zat710@users.noreply.github.com>
2021-12-08 07:54:43 +0700

    Update and rename CONFIG.js to CONFIG2.js

---
+++ CONFIG2.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Sinh nhật vui vẻ nha mẹ gà :)",
+  descriptionHeader:
+    "Quà này hơi phèn nhưng mong mẹ gà sẽ nhận ^^",
+  buttonYes: "Click vào để xem quà :')",
+  buttonNo: "Nope, quá phèn. Không thèm coi!",
+  titleModar: "Happy BirthDay!!",
+  descriptionModar:
+    'Chúc mẹ gà có 1 ngày sn thật vui vẻ ^^ </br> Chúc mẹ gà sẽ lun vui vẻ, xênh xẻo, và thành công trong cả học tập lẫn tương lai </br>',
+};
